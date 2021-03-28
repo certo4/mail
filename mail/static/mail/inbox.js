2844a05dc329d892774f00737c2eebcfa2499457
Camila Rovalino <camila.rovalino@gmail.com>
2021-03-27 20:03:36 -0400

    Send mail and mailbox completed

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -1,3 +1,5 @@
+//TODO: Change tab from 4 to 2
+
 document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
@@ -8,12 +10,18 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // By default, load the inbox
   load_mailbox('inbox');
+
+  // Function to send email
+  const composeForm = document.querySelector('#compose-form');
+  composeForm.addEventListener('submit', event => sendEmail(event));
+
 });
 
 function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -26,8 +34,95 @@ function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
+  document.querySelector('#email-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+  
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+      // Print emails
+      console.log(emails);
+
+      const emailsView = document.querySelector('#emails-view');
+
+      if (emails.length === 0) {
+        emailsView.innerHTML += "No emails found";
+      } else {
+        emails.forEach(addEmailToView);
+      }
+  });
+
 }
+
+function addEmailToView(email) {
+  const row = document.createElement('div');
+
+  row.classList.add('row-style');
+
+  if (email.read) {
+    row.classList.add('read');
+  }
+  
+  row.innerHTML = `<strong>${email.sender}</strong> ${email.subject} <span>${email.timestamp}</span>`;
+  
+  // On email click
+  row.addEventListener('click', () => {
+    viewEmail(email.id);
+  });
+  
+  document.querySelector('#emails-view').append(row);
+}
+
+function viewEmail(id) {
+  // Show single email view and hide other views
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#email-view').style.display = 'block';
+  document.querySelector('#compose-view').style.display = 'none';
+
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+      // Print email
+      console.log(email);
+
+      emailDetails(email);
+  });
+  
+}
+
+function emailDetails(email) {
+  const emailView = document.querySelector('#email-view');
+  
+
+}
+
+function sendEmail(event) {
+  event.preventDefault();
+
+  // Getting all user input to send to API
+  const formRecipients = document.querySelector('#compose-recipients').value;
+  const formSubject = document.querySelector('#compose-subject').value;
+  const formBody = document.querySelector('#compose-body').value;
+
+  fetch('/emails', {
+    method: 'POST',
+    body: JSON.stringify({
+        recipients: formRecipients,
+        subject: formSubject,
+        body: formBody
+    })
+    })
+    .then(response => response.json())
+    .then(result => {
+        // Print result
+        console.log(result);
+    });
+
+      //TODO: Redirect to the sent inbox
+}
+
+
+
